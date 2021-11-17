149cdf17cf40615bcb60a46fc9ef6a7ad832def9
ed85w <ed.walker@hotmail.co.uk>
2021-11-17 11:41:42 +0000

    after lesson 7

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log("hello ", e);
+  }
+
+  const handleClickAgain = (name) => {
+    console.log("hello " + name);
+  }
+  const handleClickWithEvent = (name, e) => {
+    console.log("hello " + name, e);
+  }
+
   return ( 
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click</button>
+      <button onClick={() => handleClickAgain("mario")}>Click Again</button>
+      <button onClick={(e) => handleClickWithEvent("mario", e)}>Click Thrice</button>
     </div>
    );
 }
