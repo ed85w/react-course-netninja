6d497d78271adef09c674d333f2a5b52e2536927
ed85w <ed.walker@hotmail.co.uk>
2021-11-25 13:36:26 +0000

    after lesson 14

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,37 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
+
 const Home = () => {
 
-  const handleClick = (e) => {
-    console.log("hello ", e);
+  //array destructuring used
+  // 'blogs' is var 'setBlogs' is function used to change value of var
+  const [blogs, setBlogs] = useState([
+    {title: "first blog", body: "bfu uif we iweiuh ehro", author: "mario", id: 1},
+    {title: "second blog", body: "poipoi poipoi poi poi poipoi", author: "luigi", id: 2},
+    {title: "third blog", body: "wqeqweqwwee weqw eqwee wewqe", author: "yoshi", id: 3},
+    {title: "fourth blog", body: "vbnvbn vbnv bn vn bvnbvn e", author: "mario", id: 4}
+  ]);
+
+
+  // 'functions'
+  const handleDelete = (id) => {
+    // create new array with filter applied
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    // use setBlogs to update original 'blogs' variable to the value of the newly created array 
+    setBlogs(newBlogs);
   }
 
-  const handleClickAgain = (name) => {
-    console.log("hello " + name);
-  }
-  const handleClickWithEvent = (name, e) => {
-    console.log("hello " + name, e);
-  }
+  // runs on every render (load, state changed etc)
+  useEffect(() => {
+    console.log("use effect");
+    console.log(blogs);
+  })
 
   return ( 
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click</button>
-      <button onClick={() => handleClickAgain("mario")}>Click Again</button>
-      <button onClick={(e) => handleClickWithEvent("mario", e)}>Click Thrice</button>
+      <BlogList blogs={ blogs } title="this is the title!!!" handleDelete={handleDelete} />
+      <BlogList blogs={ blogs.filter((blog) => blog.author === "mario" )} title="Mario's Blogs" />
     </div>
    );
 }
