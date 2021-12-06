78400286bda7ef32c4faa6cc48b40c88125b79d7
ed85w <ed.walker@hotmail.co.uk>
2021-12-06 15:37:34 +0000

    after error handling api calls lesson

--- src/Home.js
+++ src/Home.js
@@ -5,24 +5,34 @@ import BlogList from "./BlogList";
 const Home = () => {
 
   //array destructuring used
-  // 'blogs' is var 'setBlogs' is function used to change value of var
+  // 'blogs' is variable 'setBlogs' is function used to change value of variable, null is initial value
   const [blogs, setBlogs] = useState(null);
-
   const [isLoading, setIsLoading] = useState(true); 
+  const [error, setError] = useState(null);
 
   // runs on every render (load, state changed etc)
   useEffect(() => {
     // fetch data from 
     fetch('http://localhost:8000/blogs')            // cmd to run json server = npx json-server --watch data/db.json --port 8000
-    // then convert response to json
     .then(response => {
+      // if the response is not ok, throw error 
+      if(!response.ok) {
+        throw Error("whoopsie!");
+      }
+      console.log(response)
+      // then convert response to json
       return response.json();
+
     })
     // then set 'blogs' variable to value of converted response 
     .then(data => {
-      console.log(data);
       setBlogs(data);
-      setIsLoading(false)
+      setIsLoading(false);
+      setError(null);
+    })
+    .catch(err => {
+      setIsLoading(false); //so user doesn't see loading div
+      setError(err.message); 
     })
   }, [])  
 
@@ -31,6 +41,7 @@ const Home = () => {
       {/* outputs a div if isLoading = true */}
       { isLoading && <div>Loading...</div> }
 
+      { error && <div> {error} </div> }
 
       {/* '{blogs && ... }' required to allow for fetch of data from 'api' (check if data exists) */}
       {blogs &&  <BlogList blogs={ blogs } title="this is the title!!!" />}
