114ae7d3aa3cfe1d5209962ffbd46d255814dc77
ed85w <ed.walker@hotmail.co.uk>
2021-12-06 15:14:07 +0000

    after lesson 18

--- src/Home.js
+++ src/Home.js
@@ -8,10 +8,12 @@ const Home = () => {
   // 'blogs' is var 'setBlogs' is function used to change value of var
   const [blogs, setBlogs] = useState(null);
 
+  const [isLoading, setIsLoading] = useState(true); 
+
   // runs on every render (load, state changed etc)
   useEffect(() => {
     // fetch data from 
-    fetch('http://localhost:8000/blogs')
+    fetch('http://localhost:8000/blogs')            // cmd to run json server = npx json-server --watch data/db.json --port 8000
     // then convert response to json
     .then(response => {
       return response.json();
@@ -20,13 +22,17 @@ const Home = () => {
     .then(data => {
       console.log(data);
       setBlogs(data);
+      setIsLoading(false)
     })
   }, [])  
 
   return ( 
     <div className="home">
+      {/* outputs a div if isLoading = true */}
+      { isLoading && <div>Loading...</div> }
+
+
       {/* '{blogs && ... }' required to allow for fetch of data from 'api' (check if data exists) */}
-      
       {blogs &&  <BlogList blogs={ blogs } title="this is the title!!!" />}
     </div>
    );
