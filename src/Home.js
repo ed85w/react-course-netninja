b930fa259d3d7c78ebea33f33d76bce464a660b2
ed85w <ed.walker@hotmail.co.uk>
2021-12-08 16:03:03 +0000

    after lesson 24 useEffect cleanup

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
--- src/useFetch.js
+++ src/useFetch.js
@@ -10,14 +10,16 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    //stops fetch if required (eg user quickly navigates away from page before loaded)
+    const abortCont = new AbortController();
+
     // fetch data from 
-    fetch(url)            // cmd to run json server = npx json-server --watch data/db.json --port 8000
+    fetch(url, {signal: abortCont.signal})            // cmd to run json server = npx json-server --watch data/db.json --port 8000
     .then(response => {
       // if the response is not ok, throw error 
       if(!response.ok) {
         throw Error("whoopsie!");
       }
-      console.log(response)
       // then convert response to json
       return response.json();
 
@@ -29,9 +31,16 @@ const useFetch = (url) => {
       setError(null);
     })
     .catch(err => {
-      setIsLoading(false); //so user doesn't see loading div
-      setError(err.message); 
+      if (err.name === 'AbortError') {
+        console.log("fetch aborted");
+      } else {
+        setIsLoading(false); //so user doesn't see loading div
+        setError(err.message); 
+      }
     })
+
+    return () => abortCont.abort();
+
   }, [url]) //re-run if url changes
 
   return {data, isLoading, error}
